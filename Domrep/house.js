e574e67b3c735c787bcc72f41badce078b308f54
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2025-02-19 11:49:01 +0100

    Local map tiles + GPS test
    Support of local map tiles up to a resulution from up to 14. Resulution of 16 need to be added. GPS live location tracking needs to be tested!

---
+++ Domrep/house.js
@@ -0,0 +1,12 @@
+var house = {
+    "type": "FeatureCollection",
+    "name": "Houses",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "name": "Großmutter Haus" }, "geometry": { "type": "Point", "coordinates": [ -70.07712360839956, 19.645229507873328 ] } },
+    { "type": "Feature", "properties": { "name": "Bonny Adresse????" }, "geometry": { "type": "Point", "coordinates": [ -69.848980946607, 18.520539384080415 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft 26-1" }, "geometry": { "type": "Point", "coordinates": [ -70.672646159852761, 19.788455741780432 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft xx" }, "geometry": { "type": "Point", "coordinates": [ -70.683591062294155, 19.41837553157567 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft xx" }, "geometry": { "type": "Point", "coordinates": [ -70.787870071178276, 19.07542376533787 ] } }
+    ]
+    };
