2e66e0579f1109abac5ae9ce0b24cfa6a7e0e494
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2025-02-20 13:01:29 +0100

    Added new houses and locations

--- Domrep/house.js
+++ Domrep/house.js
@@ -4,9 +4,11 @@ var house = {
     "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
     "features": [
     { "type": "Feature", "properties": { "name": "Großmutter Haus" }, "geometry": { "type": "Point", "coordinates": [ -70.07712360839956, 19.645229507873328 ] } },
-    { "type": "Feature", "properties": { "name": "Bonny Adresse????" }, "geometry": { "type": "Point", "coordinates": [ -69.848980946607, 18.520539384080415 ] } },
-    { "type": "Feature", "properties": { "name": "Unterkunft 26-1" }, "geometry": { "type": "Point", "coordinates": [ -70.672646159852761, 19.788455741780432 ] } },
-    { "type": "Feature", "properties": { "name": "Unterkunft xx" }, "geometry": { "type": "Point", "coordinates": [ -70.683591062294155, 19.41837553157567 ] } },
-    { "type": "Feature", "properties": { "name": "Unterkunft xx" }, "geometry": { "type": "Point", "coordinates": [ -70.787870071178276, 19.07542376533787 ] } }
+    { "type": "Feature", "properties": { "name": "Unterkunft 06-07", "url": "https://www.airbnb.de/trips/v1/reservation-details/ro/RESERVATION2_CHECKIN/HMAS8RJN42?confirmationCode=HMAS8RJN42" }, "geometry": { "type": "Point", "coordinates": [ -70.409155179150588, 18.934748564312123 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft 26-01", "url": "https://www.airbnb.de/trips/v1/reservation-details/ro/RESERVATION2_CHECKIN/HMN9CQ4ZJB?confirmationCode=HMKACZ4SQD" }, "geometry": { "type": "Point", "coordinates": [ -70.672646159852761, 19.788455741780432 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft 01-03", "url": "https://www.airbnb.de/trips/v1/reservation-details/ro/RESERVATION2_CHECKIN/HMPWS52WT3?confirmationCode=HMKACZ4SQD58,50%20%E2%82%AC" }, "geometry": { "type": "Point", "coordinates": [ -70.683591062294155, 19.41837553157567 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft 03-06", "url": "https://www.airbnb.de/trips/v1/reservation-details/ro/RESERVATION2_CHECKIN/HMKACZ4SQD?confirmationCode=HMKACZ4SQD" }, "geometry": { "type": "Point", "coordinates": [ -70.787887329123492, 19.075448710056804 ] } },
+    { "type": "Feature", "properties": { "name": "Unterkunft 12-13", "url": "https://www.airbnb.de/trips/v1/reservation-details/ro/RESERVATION_USER_CHECKIN/HMWFF9MD29" }, "geometry": { "type": "Point", "coordinates": [ -69.305675243173468, 18.442517303497894 ] } }
     ]
-    };
+    }
+    
